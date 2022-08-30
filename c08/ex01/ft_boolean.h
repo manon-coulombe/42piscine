a0b1ea6ab999d14c3affdd4c3d753ac58db86281
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2022-08-30 12:58:03 +0200

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,13 @@
+#ifndef FT_BOOLEAN
+# define FT_BOOLEAN
+
+#include <unistd.h>
+#define t_bool int
+#define ENVEN(nbr) (nbr % 2 == 0)
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+
+#endif
